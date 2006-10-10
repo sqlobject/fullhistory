084cb485958395f1a784a54747fc0d8574ea43cd
Oleg Broytman <phd@phdru.name>
2006-10-10 16:23:53 +0000

    Fixed the bug 1567633: KeyError when creating table from MySQL database.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.7-bugfix@1998 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/mysql/mysqlconnection.py
+++ sqlobject/mysql/mysqlconnection.py
@@ -154,7 +154,7 @@ class MySQLConnection(DBAPI):
             if field == 'id':
                 continue
             colClass, kw = self.guessClass(t)
-            if self.kw['use_unicode'] and colClass is col.StringCol:
+            if self.kw.get('use_unicode') and colClass is col.StringCol:
                 colClass = col.UnicodeCol
             kw['name'] = soClass.sqlmeta.style.dbColumnToPythonAttr(field)
             kw['notNone'] = not nullAllowed
