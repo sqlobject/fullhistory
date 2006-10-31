9d1df3fbf11a21412c7d95d46abe3052ddaea77e
Oleg Broytman <phd@phdru.name>
2006-10-31 17:44:48 +0000

    A bugfix by Raphael Derosso Pereira <raphaelpereira@gmail.com> for a bug in InheritableSQLObject related to the order of tableRegistry dictionary.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.7-bugfix@2061 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/inheritance/__init__.py
+++ sqlobject/inheritance/__init__.py
@@ -38,18 +38,21 @@ class InheritableSelectResults(SelectResults):
                 if registryClass.sqlmeta.table in tablesDict:
                     #DSM: By default, no parents are needed for the clauses
                     tableRegistry[registryClass] = registryClass
-            for registryClass in allClasses:
-                if registryClass.sqlmeta.table in tablesDict:
-                    currentClass = registryClass
-                    while currentClass._parentClass:
-                        currentClass = currentClass._parentClass
-                        if tableRegistry.has_key(currentClass):
-                            #DSM: Must keep the last parent needed
-                            #DSM: (to limit the number of join needed)
-                            tableRegistry[registryClass] = currentClass
+            tableRegistryCopy = tableRegistry.copy()
+            for childClass in tableRegistryCopy:
+                if childClass not in tableRegistry:
+                    continue
+                currentClass = childClass
+                while currentClass:
+                    if tableRegistryCopy.has_key(currentClass):
+                        if currentClass in tableRegistry:
                             #DSM: Remove this class as it is a parent one
                             #DSM: of a needed children
                             del tableRegistry[currentClass]
+                        #DSM: Must keep the last parent needed
+                        #DSM: (to limit the number of join needed)
+                        tableRegistry[childClass] = currentClass
+                    currentClass = currentClass._parentClass
             #DSM: Table registry contains only the last children
             #DSM: or standalone classes
             parentClause = []
