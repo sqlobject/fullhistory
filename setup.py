67a4ded4004e3e9fcddfc455f738b0ea6fab36b1
Oleg Broytman <phd@phdru.name>
2007-03-03 10:28:09 +0000

    Going to release 0.8.1b1.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.8@2386 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if is_setuptools:
     """
 
 setup(name="SQLObject",
-      version="0.8",
+      version="0.8.1b1",
       description="Object-Relational Manager, aka database wrapper",
       long_description="""\
 SQLObject is a popular *Object Relational Manager* for providing an
