af02931439072b174966ed7ef974e4c8992afe4c
Oleg Broytman <phd@phdru.name>
2007-05-03 13:34:40 +0000

    SQLObject 0.8.3 was released 3 May 2007.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.8@2641 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if is_setuptools:
     """
 
 setup(name="SQLObject",
-      version="0.8.2",
+      version="0.8.3",
       description="Object-Relational Manager, aka database wrapper",
       long_description="""\
 SQLObject is a popular *Object Relational Manager* for providing an
