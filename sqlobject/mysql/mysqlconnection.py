173d996d614ffb32ab95f865247f7850315dcaf3
Oleg Broytman <phd@phdru.name>
2007-07-12 15:32:23 +0000

    A patch by Brian Cole <brianc@temple.edu>: MySQLConnection now recognizes Time columns when drawing the database scheme from DB.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.9@2742 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/mysql/mysqlconnection.py
+++ sqlobject/mysql/mysqlconnection.py
@@ -270,6 +270,8 @@ class MySQLConnection(DBAPI):
             return col.DateTimeCol, {}
         elif t.startswith('date'):
             return col.DateCol, {}
+        elif t.startswith('time'):
+            return col.TimeCol, {}
         elif t.startswith('timestamp'):
             return col.TimestampCol, {}
         elif t.startswith('bool'):
