0b051dffd6bceb8d1e8423a1c0539ae18a7cf30b
Oleg Broytman <phd@phdru.name>
2008-05-20 14:14:49 +0000

    Fixed a minor bug in Set column.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.9@3444 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/col.py
+++ sqlobject/col.py
@@ -920,6 +920,21 @@ class ForeignKey(KeyCol):
     def __init__(self, foreignKey=None, **kw):
         super(ForeignKey, self).__init__(foreignKey=foreignKey, **kw)
 
+
+class EnumValidator(validators.Validator):
+
+    def to_python(self, value, state):
+        if value in self.enumValues:
+            return value
+        elif not self.notNone and value is None:
+            return None
+        else:
+            raise validators.Invalid("expected a member of %r in the EnumCol '%s', got %r instead" % \
+                                     (self.enumValues, self.name, value), value, state)
+
+    def from_python(self, value, state):
+        return self.to_python(value, state)
+
 class SOEnumCol(SOCol):
 
     def __init__(self, **kw):
@@ -978,20 +993,6 @@ class SOEnumCol(SOCol):
         else:
             return len(obj)
 
-class EnumValidator(validators.Validator):
-
-    def to_python(self, value, state):
-        if value in self.enumValues:
-            return value
-        elif not self.notNone and value is None:
-            return None
-        else:
-            raise validators.Invalid("expected a member of %r in the EnumCol '%s', got %r instead" % \
-                                     (self.enumValues, self.name, value), value, state)
-
-    def from_python(self, value, state):
-        return self.to_python(value, state)
-
 class EnumCol(Col):
     baseClass = SOEnumCol
 
@@ -1027,7 +1028,7 @@ class SOSetCol(SOCol):
             super(SOSetCol, self).createValidators()
 
     def _mysqlType(self):
-        return "SET(%s)" % ', '.join([sqlbuilder.sqlrepr(v, 'mysql') for v in self.enumValues])
+        return "SET(%s)" % ', '.join([sqlbuilder.sqlrepr(v, 'mysql') for v in self.setValues])
 
 class SetCol(Col):
     baseClass = SOSetCol
