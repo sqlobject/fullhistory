4c30f9838bbb8afe44ef4151d2d3d416ced6cde3
Oleg Broytman <phd@phdru.name>
2009-03-27 16:23:58 +0000

    Fixed a minor bug - pass name to DecimalValidator.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.9@3836 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/col.py
+++ sqlobject/col.py
@@ -955,8 +955,8 @@ class SOEnumCol(SOCol):
         return [consts.isString, consts.InList(self.enumValues)]
 
     def createValidators(self):
-        return [EnumValidator(name = self.name, enumValues = self.enumValues,
-                              notNone = self.notNone)] + \
+        return [EnumValidator(name=self.name, enumValues=self.enumValues,
+                              notNone=self.notNone)] + \
             super(SOEnumCol, self).createValidators()
 
     def _mysqlType(self):
@@ -1032,7 +1032,7 @@ class SOSetCol(SOCol):
         return [consts.isString, consts.InList(self.setValues)]
 
     def createValidators(self):
-        return [SetValidator(name = self.name, setValues = self.setValues)] + \
+        return [SetValidator(name=self.name, setValues=self.setValues)] + \
             super(SOSetCol, self).createValidators()
 
     def _mysqlType(self):
@@ -1354,7 +1354,7 @@ class SODecimalCol(SOCol):
         return 'DECIMAL(%i, %i)' % (self.size, self.precision)
 
     def createValidators(self):
-        return [DecimalValidator()] + \
+        return [DecimalValidator(name=self.name)] + \
             super(SODecimalCol, self).createValidators()
 
 class DecimalCol(Col):
