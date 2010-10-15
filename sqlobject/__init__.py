4aa4d3212b78c5fa5ffd0be5b27eac78ee335feb
Oleg Broytman <phd@phdru.name>
2010-10-15 13:04:19 +0000

    Version 0.14.1.
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/0.14.1@4269 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__init__.py
+++ sqlobject/__init__.py
@@ -1,5 +1,5 @@
 """
-SQLObject 0.14
+SQLObject 0.14.1
 """
 
 from col import *
--- sqlobject/main.py
+++ sqlobject/main.py
@@ -1,6 +1,6 @@
 """
-SQLObject 0.14
---------------
+SQLObject 0.14.1
+----------------
 
 :author: Ian Bicking <ianb@colorstudy.com>
 
