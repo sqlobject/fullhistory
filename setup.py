7ea9128adf4cf24a5958baafb411a54ceb853836
Oleg Broytman <phd@phdru.name>
2010-10-15 13:03:28 +0000

    Fixed the download URL.
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/0.14@4268 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ For development see the `subversion repository
       author="Ian Bicking",
       author_email="ianb@colorstudy.com",
       url="http://sqlobject.org/devel/",
-      download_url="http://cheeseshop.python.org/pypi/SQLObject/",
+      download_url="http://cheeseshop.python.org/pypi/SQLObject/0.14",
       license="LGPL",
       packages=["sqlobject"] + ['sqlobject.%s' % package for package in subpackages],
       scripts=["scripts/sqlobject-admin"],
