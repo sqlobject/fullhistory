37cbe3e1fbc1eba8e14e44abbd6a284d8042b05d
Oleg Broytman <phd@phdru.name>
2011-03-22 16:26:50 +0000

    SQLObject 1.0.0b2.
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/1.0.0b2@4370 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from distutils.core import setup
     is_setuptools = False
 
-from sqlobject import version
+from sqlobject import version, version_info
 
 subpackages = ['firebird', 'include', 'include.pydispatch', 'inheritance',
                'manager', 'maxdb', 'mysql', 'mssql', 'postgres', 'rdbhost',
@@ -38,7 +38,7 @@ Supports MySQL, PostgreSQL, SQLite, Firebird, Sybase, MSSQL and MaxDB (SAPDB).
 
 For development see the `subversion repository
 <http://svn.colorstudy.com/SQLObject/branches/%s/>`_
-""" % version,
+""" % '.'.join([str(v) for v in version_info[:2]]),
       classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -51,7 +51,7 @@ For development see the `subversion repository
       author="Ian Bicking",
       author_email="ianb@colorstudy.com",
       url="http://sqlobject.org/",
-      download_url="http://cheeseshop.python.org/pypi/SQLObject/%s" % version,
+      download_url="http://cheeseshop.python.org/pypi/SQLObject/%sdev-r4370" % version,
       license="LGPL",
       packages=["sqlobject"] + ['sqlobject.%s' % package for package in subpackages],
       scripts=["scripts/sqlobject-admin", "scripts/sqlobject-convertOldURI"],
--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.0'
+version = '1.0.0b2'
 major   = 1
 minor   = 0
 micro   = 0
-release_level = 'branch'
-serial  = 0
+release_level = 'beta'
+serial  = 2
 version_info = (major, minor, micro, release_level, serial)
