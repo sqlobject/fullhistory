1d8b43da6a0c4986d96bf2fc79b34a11465a33e4
Oleg Broytman <phd@phdru.name>
2012-03-01 15:46:20 +0000

    Stable version 1.2.2.
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/1.2.2@4507 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.2'
+version = '1.2.2'
 major   = 1
 minor   = 2
-micro   = 0
-release_level = 'branch'
+micro   = 2
+release_level = 'final'
 serial  = 0
 version_info = (major, minor, micro, release_level, serial)
